764eaa470512ae4317220806260fd6888444c8b9
Ishant <223127347+IshantOnIt@users.noreply.github.com>
2025-08-03 23:40:37 +0530

    add dummy UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>0</Text>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.button}>
+          <Pressable onPress={() => alert("Pressed")}>
+            <MaterialIcons name="add" size={32} color="#333" />
+          </Pressable>
+        </View>
+        <View style={styles.button}>
+          <Pressable onPress={() => alert("Pressed")}>
+            <MaterialIcons name="remove" size={32} color="#333" />
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 96,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#eee",
+    marginBottom: 16,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 24,
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#eee",
+    padding: 4,
+    borderRadius: 4,
+  },
+});
