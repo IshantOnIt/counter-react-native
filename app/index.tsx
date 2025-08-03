ccbdaeff03c632eddf69932e0ad03e570e6289a0
Ishant <223127347+IshantOnIt@users.noreply.github.com>
2025-08-03 23:43:48 +0530

    add counter increment decrement functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,29 @@
 import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>0</Text>
+      <Text style={styles.title}>{count}</Text>
       <View style={styles.buttonsContainer}>
         <View style={styles.button}>
-          <Pressable onPress={() => alert("Pressed")}>
+          <Pressable onPress={increment}>
             <MaterialIcons name="add" size={32} color="#333" />
           </Pressable>
         </View>
         <View style={styles.button}>
-          <Pressable onPress={() => alert("Pressed")}>
+          <Pressable onPress={decrement}>
             <MaterialIcons name="remove" size={32} color="#333" />
           </Pressable>
         </View>
